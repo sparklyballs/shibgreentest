06c85cd5c883b6c7d59a684bc2d84d04aab847a6
sparklyballs <sparklyballs@gmail.com>
2021-11-09 21:02:01 +0000

    first save up

---
+++ docker-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2154
+if [[ -n "${TZ}" ]]; then
+  echo "Setting timezone to ${TZ}"
+  ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
+fi
+
+cd /shibgreen-blockchain || exit 1
+
+# shellcheck disable=SC1091
+. ./activate
+
+shibgreen init --fix-ssl-permissions
+
+if [[ ${testnet} == 'true' ]]; then
+   echo "configure testnet"
+   shibgreen configure --testnet true
+fi
+
+if [[ ${keys} == "persistent" ]]; then
+  echo "Not touching key directories"
+elif [[ ${keys} == "generate" ]]; then
+  echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
+  shibgreen keys generate
+elif [[ ${keys} == "copy" ]]; then
+  if [[ -z ${ca} ]]; then
+    echo "A path to a copy of the farmer peer's ssl/ca required."
+	exit
+  else
+  shibgreen init -c "${ca}"
+  fi
+else
+  shibgreen keys add -f "${keys}"
+fi
+
+for p in ${plots_dir//:/ }; do
+    mkdir -p "${p}"
+    if [[ ! $(ls -A "$p") ]]; then
+        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+    fi
+    shibgreen plots add -d "${p}"
+done
+
+if [[ -n "${log_level}" ]]; then
+  shibgreen configure --log-level "${log_level}"
+fi
+
+sed -i 's/localhost/127.0.0.1/g' "$CONFIG_ROOT/config/config.yaml"
+
+exec "$@"
